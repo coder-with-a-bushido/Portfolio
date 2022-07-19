c26fef7b25a957de64672014c1b21ecd7ba3fb86
Karthikeyan S <karthikeyanssvk@gmail.com>
2022-07-19 12:32:21 +0530

    change resume pdf to png

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,15 +1,11 @@
+import { onClickUrl } from '@site/src/utils';
 import React from 'react';
 import styles from './styles.module.css';
 
 export default function HomepageFeatures(): JSX.Element {
   return (
-    <div className="relative h-0 overflow-hidden max-w-full w-full" 	>
-          <object
-            data={"/Karthikeyan S - Resume.pdf#view=fit"}
-            type="application/pdf"
-            width="100%"
-            height="900px"
-          />
+    <div className={styles.centered}>
+          <img src="/Karthikeyan_S-Resume.png" onClick={onClickUrl("/Karthikeyan_S-Resume.pdf")} className={styles.resume}/>
     </div>
   );
 }
