8701abe1b03ee29e01de773a064a81f6616fbecb
Karthikeyan S <karthikeyanssvk@gmail.com>
2022-10-05 20:58:22 +0530

    add meet link redirect

---
+++ src/pages/meet/admin.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MeetRedirect(): JSX.Element {
+  React.useEffect(() => {
+    window.location.replace("https://bushi.app.100ms.live/meeting/udq-mzg-zxg");
+  }, []);
+
+  return <div></div>;
+}
---
+++ src/pages/meet/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MeetRedirect(): JSX.Element {
+  React.useEffect(() => {
+    window.location.replace("https://bushi.app.100ms.live/meeting/sxf-arp-khy");
+  }, []);
+
+  return <div></div>;
+}
